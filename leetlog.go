7225a3a1b5a41a73b853eedab9509ee62cf0ef50
Ulisse Mini <ulisse.mini@gmail.com>
2019-02-06 13:32:06 -0500

    Refactor

--- leetlog.go
+++ leetlog.go
@@ -12,6 +12,56 @@ var (
 	debugPrefix = "[~] "
 )
 
+type logger struct {
+	out   *log.Logger // output logs for the user
+	info  *log.Logger // info level logs
+	warn  *log.Logger // warning level logs
+	err   *log.Logger // error level logs
+	debug *log.Logger // debug level logger
+}
+
+// Informational logging
+func (l *logger) Infof(format string, a ...interface{}) {
+	l.info.Printf(format, a...)
+}
+
+// Informational logging
+func (l *logger) Info(format string, a ...interface{}) {
+	l.info.Printf(format, a...)
+}
+
+// Output (for the user)
+func (l *logger) Printf(format string, a ...interface{}) {
+	l.out.Printf(format, a...)
+}
+
+// Output (for the user)
+func (l *logger) Print(a ...interface{}) {
+	l.out.Print(a...)
+}
+
+// Error logging
+func (l *logger) Errorf(format string, a ...interface{}) {
+	l.err.Printf(format, a...)
+}
+
+// Error logging
+func (l *logger) Error(a ...interface{}) {
+	l.err.Print(a...)
+}
+
+// Default logger methods
+
+// Warning level logs
+func (l *logger) Warnf(format string, a ...interface{}) {
+	l.warn.Printf(format, a...)
+}
+
+// Warning level logs
+func (l *logger) Warn(a ...interface{}) {
+	l.warn.Print(a...)
+}
+
 // Debug level logging
 func Debugf(format string, a ...interface{}) {
 	defaultLogger.debug.Printf(format, a...)
@@ -60,52 +110,4 @@ func Warnf(format string, a ...interface{}) {
 // Warning level logs
 func Warn(a ...interface{}) {
 	defaultLogger.warn.Print(a...)
-}
-
-type logger struct {
-	out   *log.Logger // output logs for the user
-	info  *log.Logger // info level logs
-	warn  *log.Logger // warning level logs
-	err   *log.Logger // error level logs
-	debug *log.Logger // debug level logger
-}
-
-// Informational logging
-func (l *logger) Infof(format string, a ...interface{}) {
-	l.info.Printf(format, a...)
-}
-
-// Informational logging
-func (l *logger) Info(format string, a ...interface{}) {
-	l.info.Printf(format, a...)
-}
-
-// Output (for the user)
-func (l *logger) Printf(format string, a ...interface{}) {
-	l.out.Printf(format, a...)
-}
-
-// Output (for the user)
-func (l *logger) Print(a ...interface{}) {
-	l.out.Print(a...)
-}
-
-// Error logging
-func (l *logger) Errorf(format string, a ...interface{}) {
-	l.err.Printf(format, a...)
-}
-
-// Error logging
-func (l *logger) Error(a ...interface{}) {
-	l.err.Print(a...)
-}
-
-// Warning level logs
-func (l *logger) Warnf(format string, a ...interface{}) {
-	l.warn.Printf(format, a...)
-}
-
-// Warning level logs
-func (l *logger) Warn(a ...interface{}) {
-	l.warn.Print(a...)
 }
